f37ba592db987fef0fb5777223298f51e794299f
백인기 <baekinki2026@gmail.com>
2026-02-12 21:48:57 +0900

    com

--- A_test/make_num/main.py
+++ A_test/make_num/main.py
@@ -2,22 +2,52 @@ import sys
 sys.stdin = open('sample_input.txt','r')
 
 '''
-연산자 객체를 순회?
-
+부분조합, 백트래킹을 해야하는 문제
+다시 풀어보기
 '''
+
+from collections import deque
+
+def generate_subset(deps):
+    global min_val, max_val
+
+    if deps == N-1:
+        min_val = min(min_val,numbers[0])
+        max_val = max(max_val,numbers[0])
+        return
+
+    for i in range(4):
+        if operates[i] == 0: continue
+        operates[i] -= 1
+        op = oper[i]
+        n1 = numbers.popleft()
+        n2 = numbers.popleft()
+        if op == '+':
+            numbers.appendleft(n1 + n2)
+        elif op == '-':
+            numbers.appendleft(n1 - n2)
+        elif op == '*':
+            numbers.appendleft(n1 * n2)
+        elif op == '/':
+            numbers.appendleft(int(n1 / n2))
+        generate_subset(deps+1)
+        #백트래킹
+        operates[i] += 1
+        numbers.popleft()
+        numbers.appendleft(n2)
+        numbers.appendleft(n1)
 
 T = int(input())
 for tc in range(1,T+1):
     N = int(input())
     operates = list(map(int,input().split()))
-    numbers = list(map(int,input().split()))
+    numbers = deque(list(map(int,input().split())))
 
     oper = ['+','-','*','/']
-    op_st = ''
-    for i in range(4):
-        op_st += oper[i] * operates[i]
-
-    #print(op_st)
+    max_val = float('-inf')
+    min_val = float('inf')
+    generate_subset(0)
+    print(f'#{tc} {max_val-min_val}')
 
 
 
---
+++ A_test/make_num/test.py
@@ -0,0 +1,50 @@
+from collections import deque
+ 
+maps = {
+    0 : '+',
+    1 : '-',
+    2 : '*',
+    3 : '/',
+}
+ 
+def permutations(oper_arr, number_q, n, N, values):
+    if n == N-1:
+        values['max'] = max(values['max'], number_q[0])
+        values['min'] = min(values['min'], number_q[0])
+        return
+    for i in range(4):
+        if oper_arr[i] == 0: continue
+        oper_arr[i] -= 1
+        n1 = number_q.popleft()
+        n2 = number_q.popleft()
+ 
+        if maps[i] == '+':
+            number_q.appendleft(n1+n2)
+        elif maps[i] == '-':
+            number_q.appendleft(n1-n2)
+        elif maps[i] == '*':
+            number_q.appendleft(n1 * n2)
+        else:
+            number_q.appendleft(int(n1/n2))
+        permutations(oper_arr, number_q, n+1, N, values)
+        number_q.popleft()
+        number_q.appendleft(n2)
+        number_q.appendleft(n1)
+        oper_arr[i] += 1
+ 
+ 
+def solve():
+    T = int(input())
+    for test_case in range(1, T + 1):
+        N = int(input())
+        oper_arr = list(map(int, input().split()))
+        number_arr = list(map(int, input().split()))
+        number_q = deque(number_arr)
+        values = {
+            'max' : -float('inf'),
+            'min' : float('inf')
+        }
+        permutations(oper_arr, number_q, 0, N, values)
+        print(f"#{test_case} {values['max']-values['min']}")
+ 
+solve()
