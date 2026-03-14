c22c171423dc557fd48dd39bd6f29b742af662aa
백인기 <baekinki2026@gmail.com>
2026-03-14 09:40:00 +0900

    set

--- B_first_practice/cafe_n_bakery/solution.py
+++ B_first_practice/cafe_n_bakery/solution.py
@@ -1,36 +1,43 @@
-from heapq import heappop,heappush
 from collections import defaultdict
+from heapq import heappop, heappush
 
+# 건물 개수
 build_n = 0
-building_roads = defaultdict(list) # 건물주소 : [ (거리, 목표건물주소), (거리, 목표건물주소), ... ]
+# 건물 도로
+building_roads = defaultdict(list) # 건물ID : [ (거리, 목표건물ID),(거리, 목표건물ID),(거리, 목표건물ID) ]
 
-# 시작지점 st_n 부터 각 지점까지의 최단거리 리턴
-def dijkstra(st_n):
-	fast_paths = [float('inf')] * build_n
-	# fast_paths[st_n] = 0
-	hq = [(0, st_n)]
+
+def dijkstra(target_list, R):
+	min_dist_list = [float('inf')] * build_n
+	hq = []
+	for _id in target_list:
+		min_dist_list[_id] = 0
+		heappush(hq, (0, _id))
+
 	while hq:
-		dist, pos = heappop(hq)
-		if dist > fast_paths[pos]:
-			continue
-		for next_dist, next_pos in building_roads[pos]:
+		dist, node = heappop(hq)
+		if dist > R: break
+		if dist > min_dist_list[node]: continue
+
+		for next_dist, next_id in building_roads[node]:
 			total_dist = dist + next_dist
-			if next_pos == st_n: continue
-			if total_dist < fast_paths[next_pos]:
-				fast_paths[next_pos] = total_dist
-				heappush(hq, (total_dist, next_pos))
-	return fast_paths
+			if total_dist < min_dist_list[next_id]:
+				min_dist_list[next_id] = total_dist
+				if total_dist <= R:
+					heappush(hq, (total_dist, next_id))
+
+	return min_dist_list
 
 
 def init(N, K, sBuilding, eBuilding, mDistance):
 	global build_n, building_roads
-
 	build_n = N
 	building_roads = defaultdict(list)
 
 	for i in range(K):
-		building_roads[sBuilding[i]].append((mDistance[i], eBuilding[i]))
-		building_roads[eBuilding[i]].append((mDistance[i], sBuilding[i]))
+		b_1, b_2, dist = sBuilding[i], eBuilding[i], mDistance[i]
+		building_roads[b_1].append((dist, b_2))
+		building_roads[b_2].append((dist, b_1))
 
 
 def add(sBuilding, eBuilding, mDistance):
@@ -39,35 +46,19 @@ def add(sBuilding, eBuilding, mDistance):
 
 
 def calculate(M, mCoffee, P, mBakery, R):
-	coffee_set = set(mCoffee)
-	bakery_set = set(mBakery)
+	cafe_dist = dijkstra(mCoffee, R)
+	bake_dist = dijkstra(mBakery, R)
 
-	cafe_dists = [dijkstra(cafe) for cafe in mCoffee]
-	bakery_dists = [dijkstra(bakery) for bakery in mBakery]
-
-	min_dist_total = float('inf')
+	not_home_build_ids = set(mCoffee + mBakery)
+	min_dist = float('inf')
 
 	for i in range(build_n):
-		if i in coffee_set or i in bakery_set:
+		if i in not_home_build_ids:
 			continue
-		min_cafe = float('inf')
-		min_bake = float('inf')
+		if cafe_dist[i] <= R and bake_dist[i] <= R:
+			min_dist = min(min_dist, cafe_dist[i] + bake_dist[i])
 
-		for c_dist_list in cafe_dists:
-			if c_dist_list[i] <= R:
-				min_cafe = min(min_cafe, c_dist_list[i])
-
-		for b_dist_list in bakery_dists:
-			if b_dist_list[i] <= R:
-				min_bake = min(min_bake, b_dist_list[i])
-
-		if min_cafe == float('inf') or min_bake == float('inf'):
-			continue
-
-		dist_total = min_cafe + min_bake
-		min_dist_total = min(min_dist_total, dist_total)
-
-	if min_dist_total == float('inf'):
+	if min_dist == float('inf'):
 		return -1
 	else:
-		return min_dist_total
+		return min_dist
---
+++ B_first_practice/paint_2/solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+
+
+def init(N : int, L : int, mCode : List[str]) -> None:
+	pass
+
+def encode(mCode : List[str]) -> int:
+	return -1
+
+def makeDot(mR : int, mC : int, mSize : int, mColor : int) -> None:
+	pass
+
+def paint(mR : int, mC : int, mColor : int) -> None:
+	pass
+
+def getColor(mR : int, mC : int) -> int:
+	return -1
